efea925852c86eb9739e8272068124adb0de1d20
Jonathan Willing <jwilling@me.com>
2015-04-17 19:25:38 -0500

    get a basic map displaying

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 // main.js
 
 $(document).ready(function() {
-    
+    initialize();
 });
+
+function initialize() {
+    // Initialize the map.
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
